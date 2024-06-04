f4697ff76dc47719eed67c5f76ccfb8bd4aced65
hui890514 <hui890514@gmail.com>
2024-06-04 11:06:15 +0800

    chore: move css in App.vue outside

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,3 @@ import { RouterView } from 'vue-router'
 <template>
   <RouterView />
 </template>
-
-<style>
-*{
-	box-sizing: border-box;
-	margin: 0;
-	padding: 0;
-}
-html, body, #app{
-	width: 100%;
-	height: 100%;
-	overflow: hidden;
-}
-*::-webkit-scrollbar {
-	display: none;
-}
-</style>
