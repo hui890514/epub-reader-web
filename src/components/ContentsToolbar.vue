127929d10b690ecf922949f076cb1b93d250e613
hui890514 <hui890514@gmail.com>
2024-06-05 15:43:49 +0800

    feat: refactor contents

---
+++ src/components/ContentsToolbar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { collapseAll } from '@/helpers/contents'
+</script>
+
+<template>
+  <div>
+    <div>
+      Contents
+    </div>
+    <div f-r-n items-center>
+      <div i-d title="collapse all" @click="collapseAll(true)">
+        <div i-mdi:collapse-all-outline c-t />
+      </div>
+      <div i-d title="expand all" @click="collapseAll(false)">
+        <div i-mdi:expand-all-outline c-t />
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/ReaderView.vue
+++ src/views/ReaderView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import Toolbar from '@/components/Toolbar.vue'
 import Contents from '@/components/Contents.vue'
+import ContentsPage from '@/components/ContentsPage.vue'
+import ContentsToolbar from '@/components/ContentsToolbar.vue'
 import Reader from '@/components/Reader.vue'
 import { contents } from '@/helpers/reader'
 </script>
@@ -12,10 +14,13 @@ import { contents } from '@/helpers/reader'
     </div>
     <div f-r-n flex-1 overflow-hidden>
       <div
-        w-75 border-0 border-r-2 border-solid border-t overflow-hidden overflow-y-auto p-1
-        class="contents-wrapper"
+        w-75 border-0 border-r-2 border-solid border-t overflow-hidden f-c-n
       >
-        <Contents :contents="contents" />
+        <ContentsToolbar h-9 border-0 border-b-2 border-solid border-t p-1 f-r-n items-center justify-between />
+        <div flex-1 overflow-auto p-1 class="contents-wrapper">
+          <Contents :contents="contents" />
+        </div>
+        <ContentsPage h-9 border-0 border-t-2 border-solid border-t p-1 f-r-n items-center />
       </div>
       <div flex-1 overflow-hidden>
         <Reader />
