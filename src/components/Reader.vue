262f9201ee92382bdaaaf5207485d1072ab92f3a
hui890514 <hui890514@gmail.com>
2024-06-07 13:53:10 +0800

    chore: add test book

--- src/components/Reader.vue
+++ src/components/Reader.vue
@@ -2,7 +2,8 @@
 import { onMounted } from 'vue'
 import { showEpub } from '@/helpers/reader'
 
-const url = '/The Almanack of Naval Ravikant.epub'
+// const url = '/The Almanack of Naval Ravikant.epub'
+const url = '33.epub'
 
 onMounted(() => {
   showEpub(url)
