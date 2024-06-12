66343c4c6a46d184b8a08b6df025e91ae27017fb
hui890514 <hui890514@gmail.com>
2024-06-12 17:59:57 +0800

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { URL, fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import UnoCSS from 'unocss/vite'
+import unoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    UnoCSS(),
+    unoCSS(),
   ],
   resolve: {
     alias: {
