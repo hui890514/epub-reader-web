97a099363046d56e0b520f51576a7c08f9ca5d91
hui890514 <hui890514@gmail.com>
2024-06-12 16:15:18 +0800

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './epub-book-reader/',
+  base: '/epub-book-reader/',
   build: {
     target: 'esnext',
   },
